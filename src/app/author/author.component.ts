5eaecb56d0f8fc9e45aadb29627b079764011da2
ironarmchad <mcline@muralab.org>
2019-08-26 18:54:37 -0400

    basic component and service studied

---
+++ src/app/author/author.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthorService} from '../author.service';
+
+@Component({
+  selector: 'app-author',
+  templateUrl: './author.component.html',
+  styleUrls: ['./author.component.css']
+})
+export class AuthorComponent {
+    authors;
+
+    constructor(service: AuthorService) {
+        this.authors = service.getAuthors();
+    }
+
+}
